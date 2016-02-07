a058d1f71593c89e6f6915fcbbc428c05bf3b388
ferraricharles <charles.cjf@gmail.com>
2016-02-07 14:39:26 +0100

    heroku config

--- src/ucoach/auth/dao/AuthDao.java
+++ src/ucoach/auth/dao/AuthDao.java
@@ -1,4 +1,8 @@
 package ucoach.auth.dao;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -6,14 +10,46 @@ import javax.persistence.Persistence;
 
 public enum AuthDao {
     instance;
-    private EntityManagerFactory emf;
+    private EntityManagerFactory emf;    
+    
+    private Map<String, String> getDBProperties(){
+     String databaseUrl = System.getenv("CLEARDB_DATABASE_URL");
+     Map<String, String> properties = new HashMap<String, String>();
+ 
+     if( databaseUrl != null ) {
+       StringTokenizer st = new StringTokenizer(databaseUrl, ":@/");
+       String dbVendor = st.nextToken();
+       String username = st.nextToken();
+       String password = st.nextToken();
+       String host = st.nextToken();
+       String databaseName = st.nextToken();
+       String jdbcUrl = String.format("jdbc:%s://%s/%s", dbVendor, host, databaseName);
+       properties.put("hibernate.connection.url", jdbcUrl);
+       properties.put("hibernate.connection.username", username);
+       properties.put("hibernate.connection.password", password);
+ 
+     } else {
+       properties.put("hibernate.connection.url", "jdbc:mysql://127.0.0.1:3306/auth");
+       properties.put("hibernate.connection.username", "root");
+       properties.put("hibernate.connection.password", "root");
+     }
+ 
+     return properties;
+   }
 
-    private AuthDao() {
+   private AuthDao() {
+    
         if (emf!=null) {
             emf.close();
         }
-        emf = Persistence.createEntityManagerFactory("authentication-jpa");
+        
+        try{
+          emf = Persistence.createEntityManagerFactory("authentication-jpa", getDBProperties());
+        }catch(Exception e){
+          System.out.println(e);
+        }   
     }
+
 
     public EntityManager createEntityManager() {
         try {
